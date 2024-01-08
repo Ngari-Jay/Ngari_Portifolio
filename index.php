4bcb2a9822f8a6cb9d23b7c409807ef5b9e90e7e
NGARI JACKSON <116723897+Ngari-Jay@users.noreply.github.com>
2024-01-08 17:56:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage | Jackson Ngari Yaa</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <!---------------------------navigation section starts here -------------------->
+<nav>
+<!---------------------------navigation left section-------------------->
+        <div class="nav-left">
+            <a href="index.php">
+            <img src="images/passport3.png" class="logo"></a>
+            <ul>
+                <a href=""><li><img src="images/notification.png"></a></li>
+                <a href=""><li><img src="images/inbox.png"></a></li>
+                <a href=""><li><img src="images/video.png"></a></li>
+                <li><a href="">Resume</a></li>
+            </ul>
+        </div>
+    </div>
+<!---------------------------navigation right section-------------------->
+    <div class="nav-right">   
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Education</a></li>
+            <li><a href="">Work | Projects</a></li>
+            <li><a href="">Contacts</a></li>
+            <li><a href="">My Referees</a></li>
+        </ul>
+    </div>
+</nav>
+<!---------------------------navigation section ends here -------------------->
+
+<!----------------------------About me Section Starts here--------------------->
+
+<section class="about">
+        <h1>About Me</h1>
+        <p class="title1">Whom I Am</p>
+
+        <div class="row">
+            <div class="about-left">
+                <h3>Contents</h3>
+                <p>The PHD and Masters/Doctorate level courses are only offered at the main campus which involves both online, full time and part time for employed persons.</p>
+                <br/>
+                <a href="#" class="hero-btn">Hire Me</a> 
+                <a href="#" class="hero-btn">view Resume</a> 
+                <br><br/>
+
+              <div class="social-media-accounts">
+                        <ul>
+                               <a href=""><li><img src="images/github.png" width="40"height="40"></a></li> 
+                               <a href=""><li><img src="images/inbox.png"></a></li> 
+                               <a href=""><li><img src="images/video.png"></a></li> 
+                               <a href=""><li><img src="images/inbox.png"></a></li>
+                               <a href=""><li><img src="images/inbox.png"></a></li>
+                               <a href=""><li><img src="images/inbox.png"></a></li>
+                               <a href=""><li><img src="images/inbox.png"></a></li>
+                        
+            
+                        </ul>
+              </div>
+            </div>
+
+            <div class="about-right">
+                <h3>My Passport Image</h3>
+                <img src="images/passport3.png">
+            </div>
+            </div>
+        </div>
+    </section>
+<!----------------------------About me Section ends here--------------------->
+
+<!----------------------------Skills Section starts here--------------------->
+<section class="skills">
+<h1>My Skills</h1>
+<p class="title1">What I Know</p>
+<div class="row">
+<div class="skills-left">
+<h3>My creative & Proffessional Skills.</h3>
+<p>Loremipsum dolor sit ametconsecteturadipisicingelit. Dignissimos, ratione error estrecusandaeconsequatur, iustoillumdelenitiquidemimpedit, quos quaeratquis minima sequi. Cupiditaterecusandaelaudantiumesse, harum animi aspernaturquisquametdelectusipsum quam alias quaerat? Quasi hic quidemillum. Ad delectusnatusaut hic explicabo minus quod.</p>
+<a href="#" class="hero-btn">Read more</a>
+</div>
+
+<div class="skills-right">
+<div class="bars">
+<div class="info"><span>HTML</span><span>90%</span></div>
+<div class="line html">
+</div>
+</div>
+<div class="bars">
+<div class="info"><span>PHP</span><span>80%</span></div>
+<div class="line php">
+</div>
+</div>
+<div class="bars">
+<div class="info"><span>MySQL Database</span><span>75%</span></div>
+<div class="line mysql">
+</div>
+</div>
+<div class="info"><span>Networking</span><span>55%</span></div>
+<div class="line networking">
+</div>
+</div>
+</section>
+<!----------------------------Skills Section Ends here--------------------->
+<!-------------------------My Education section Starts Here--------------->
+
+<section class="education">
+<h1>My Education</h1>
+<p class="title1">My Academic Journey</p>
+<div class="row">
+  <!------lets create table--------->
+<table class= "tbl-full">
+    <tr>
+                            <!------lets create table headers--------->
+                            <th>Study Duration</th>
+                            <th>Institution Name</th>
+                            <th>Website Link</th>
+                            <th>Course</th>
+                            <th>Level</th>
+                            <th>Examination Board</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr>
+                            <!------lets create table data--------->
+                            <td>2021 - 2023</td>
+                            <td>Bureti Technical Training Institute</td>
+                            <td><a href="www.buretitechnical.com">www.buretitechnical.com</a></td>
+                            <td> ICT</td>
+                            <td>Diploma</td>
+                            <td>KNEC</td>
+                            <td>
+                                 <!------create two buttons--------->
+                                <a href="#" class="hero-btn">View</a>
+                                <a href="#" class="hero-btn">Download</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <!------lets create table data--------->
+                            <td>2018 - 2019</td>
+                            <td>Mekatilili Memorial Secondary</td>
+                            <td><a href="www.meketililimemorialsecondaryschool.com">www.mekatililimemorial.com</a></td>
+                            <td>KCSE</td>
+                            <td>KCSE Certificate</td>
+                            <td>KNEC</td>
+                            <td>
+                                 <!------create two buttons--------->
+                                <a href="#" class="hero-btn">View</a>
+                                <a href="#" class="hero-btn">Download</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <!------lets create table data--------->
+                            <td>2004 - 2013</td>
+                            <td>Pishimwenga Primary School</td>
+                            <td><a href="www.pishimwengaprimaryschool.com">www.pishimwengaprimaryschool.com</a></td>
+                            <td> ICT</td>
+                            <td>KCPE Certificate</td>
+                            <td>KNEC</td>
+                            <td>
+                                 <!------create two buttons--------->
+                                <a href="#" class="hero-btn">View</a>
+                                <a href="#" class="hero-btn">Download</a>
+                            </td>
+                        </tr>
+</tr>
+                     </table>
+
+    </div>
+</div
+
+
+
+
+
+
+
+
+
+</section>
+
+<!-------------------------My Education section ends Here--------------->
+
+
+</body>
+</html>
